c6d8026fe56c4c63aa17caafa12e5e1768bccc57
sambeet kumar khatua <khatuasambeet31@gmail.com>
2025-07-23 13:38:03 +0530

    fixing cors errors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,26 @@ connectCloudinary();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const allowedOrigins = [
+    'https://hcc-nitk-js2d.vercel.app/',
+   'https://hcc-nitk-qz4f.vercel.app/',
+  'http://localhost:5174',// for local development
+  'http://localhost:5173' // for local development
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const message = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 //api endpoint
 app.use('/api/admin', adminRouter);
